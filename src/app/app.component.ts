4e452be9ebcf404913b8e08d67394aebe1000389
KaraCav <karacav@msn.com>
2020-02-11 20:08:55 -0500

    changes to dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'tic-tac-toe';
+  public currentPlayer = "Blue";
+  public board = [];
+  isCool : boolean;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.startNewGame();
+  }
+  startNewGame(){
+    this.currentPlayer = "Blue";
+    this.board = [];
+  }
+}
+
+function buttMunch(){
+  
 }
